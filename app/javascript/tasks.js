a1419968529bfa23a104f999191f8c1dd2a22225
sekkey777 <seki7united@gmail.com>
2023-03-29 00:48:33 +0900

    機能修正：タスク削除時にAjaxで非同期通信（別の手法）

--- app/javascript/tasks.js
+++ app/javascript/tasks.js
@@ -26,12 +26,12 @@ document.addEventListener('turbolinks:load', function() {
 
 
 // タスク削除
-document.addEventListener('turbolinks:load', function() {
-  document.querySelectorAll('.delete').forEach(function(a) {
-    a.addEventListener('ajax:success', function() {
-      let td = a.parentNode;
-      let tr = td.parentNode;
-      tr.style.display = 'none';
-    });
-  });;
-})
+// document.addEventListener('turbolinks:load', function() {
+//   document.querySelectorAll('.delete').forEach(function(a) {
+//     a.addEventListener('ajax:success', function() {
+//       let td = a.parentNode;
+//       let tr = td.parentNode;
+//       tr.style.display = 'none';
+//     });
+//   });;
+// })
