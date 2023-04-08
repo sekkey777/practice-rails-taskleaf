ca660a88f6bdda56cb0b543467f0c0ff3a9141b5
sekkey777 <seki7united@gmail.com>
2023-04-08 23:56:42 +0900

    機能追加：いいね機能(非同期)

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,12 +1,11 @@
 class LikesController < ApplicationController
   def create
-    # binding.pry
+    @post = Post.find(params[:post_id])
     Like.create(user_id: current_user.id, post_id: params[:post_id])
-    redirect_to posts_path
   end
 
   def destroy
+    @post = Post.find(params[:post_id])
     Like.find_by(user_id: current_user.id, post_id: params[:post_id]).destroy
-      redirect_to posts_path
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,7 @@ Rails.application.routes.draw do
   end
   resources :posts do
     resources :comments
-    resources :likes, only: [:create, :destroy]
+    resource :likes, only: [:create, :destroy]
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
